3954ec8e8cef34a5c904a8f82c55fa68e1828c4b
Owner <Owner@owners-mbp.home>
2019-04-09 15:54:00 +0100

    modify route to fix typo
    Typo on category component. This fixes it.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { RecipeComponent } from './recipe/recipe.component';
 
 const routes: Routes = [
   {path: '', component: CategoriesComponent},
-  {path: 'component', component: CategoryComponent},
+  {path: 'category', component: CategoryComponent},
   {path: 'recipe', component: RecipeComponent},
   {path: '**', component: CategoriesComponent}
 ];
