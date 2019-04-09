8989ad02550f01bca3215dc6b0d40e5b12b8a030
Owner <Owner@owners-mbp.home>
2019-04-09 15:38:23 +0100

    added service to test recipe API
    Added the service necessary to test the recipe API.

--- src/app/services/recipes.service.ts
+++ src/app/services/recipes.service.ts
@@ -10,8 +10,9 @@ export class RecipesService {
     private http: HttpClient
   ) { }
   private url = 'https://api.edamam.com/search';
+  private credentials = '&app_id=8b4dd0c9&app_key=502781d3fd4f0135bc4e1ce880982d88';
 
   searchRecipes(query: string) {
-    return this.http.get(`${this.url}?q=${query}`);
+    return this.http.get(`${this.url}?q=${query+this.credentials}`);
   }
 }
