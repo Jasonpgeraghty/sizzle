3a9061ec3cb0be33e3ef9e8b1445a30e023320eb
Owner <Owner@owners-mbp.home>
2019-04-09 15:44:13 +0100

    added routes for other components
    Added routes for all pages currently created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {CategoryComponent} from './category/category.component';
+import { CategoryComponent } from './category/category.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 const routes: Routes = [
-  {path: '', component: CategoryComponent}
+  {path: '', component: CategoriesComponent},
+  {path: 'component', component: CategoryComponent},
+  {path: 'recipe', component: RecipeComponent},
+  {path: '**', component: CategoriesComponent}
 ];
 
 @NgModule({
