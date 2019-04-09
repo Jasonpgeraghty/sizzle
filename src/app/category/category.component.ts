aabc343361d2acdba9516ccbffb964424e0207c6
Owner <Owner@owners-mbp.home>
2019-04-09 16:35:22 +0100

    allow user to choose what they recipes they want to choose.
    Adds the ability for the user to choose what they would like to search for. It achieves this by using the paramMap to get the value as a GET request from the router URI.

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipesService } from '../services/recipes.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-category',
@@ -12,13 +13,22 @@ export class CategoryComponent implements OnInit {
   recipes: any;
 
   constructor(
-    private recipeService: RecipesService
+    private recipeService: RecipesService,
+    private activeRoute: ActivatedRoute,
+    private router: Router
   ) {
-    this.recipeService.searchRecipes('chicken')
+    this.activeRoute.paramMap.subscribe(params => {
+
+    const recipe = params.get('name');
+
+    this.recipeService.searchRecipes(recipe)
     .subscribe(
       (recipes: any) => {
-        this.recipes = recipes.hits;
-        console.log(recipes.hits);
+        if (recipes !== undefined) {
+          this.recipes = recipes.hits;
+          console.log(recipes.hits);
+        }
+    });
     });
   }
 
