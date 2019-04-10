408919d40ffd0d94f0fa1b6db942269fefb0c05a
Owner <Owner@Owners-MacBook-Pro.local>
2019-04-10 12:28:58 +0100

    add animations to page navigations
    Added animations to page navigations to increase the user experience. Pages now slide in from right.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import {
+  transition,
+  trigger,
+  query,
+  style,
+  animate,
+  group,
+  animateChild
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+  /* Code modifed from https://medium.com/google-developer-experts/angular-supercharge-your-router-transitions-using-new-animation-features-v4-3-3eb341ede6c8 */
+    trigger('myAnimation', [
+      transition('* <=> *', [
+        /* order */
+        /* 1 */ query(':enter, :leave', style({ position: 'fixed', width: '100%' })
+          , { optional: true }),
+        /* 2 */ group([  // block executes in parallel
+          query(':enter', [
+            style({ transform: 'translateX(100%)' }),
+            animate('0.5s ease-in-out', style({ transform: 'translateX(0%)'}))
+          ], { optional: true }),
+          query(':leave', [
+            style({ transform: 'translateX(0%)'}),
+            animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)'}))
+            ], { optional: true }),
+        ])
+      ])
+    ])
+  ] // register the animations
 })
+/* End of referenced code that has been modified */
 export class AppComponent {
   title = 'sizzle';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +28,8 @@ import { CountryComponent } from './country/country.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [
     RecipesService
