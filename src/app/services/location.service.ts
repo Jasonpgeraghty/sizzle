8e7faa4100fd9e9236d9821cc618f0b262e17dc9
Owner <Owner@Owners-MacBook-Pro.local>
2019-04-10 12:59:52 +0100

    add functionality for the user to access location
    Allow the user to get coordinates and get them from another component.

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -6,4 +6,24 @@ import { Injectable } from '@angular/core';
 export class LocationService {
 
   constructor() { }
+
+getLocation() {
+  // Event listener
+  return navigator
+  .geolocation
+  .getCurrentPosition(this.getPositionSuccess,
+    this.getPositionError,
+    {enableHighAccuracy: true});
+}
+
+// Location callbacks
+
+getPositionSuccess(position) {
+    console.log(position);
+}
+
+getPositionError(err) {
+    console.log(`Something went wrong: ${err.messages}`);
+}
+
 }
