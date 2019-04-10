087d2339bd23bf8c56945e1a8e2a143d3f10025c
Owner <Owner@Owners-MacBook-Pro.local>
2019-04-10 13:21:08 +0100

    add initialise feature to localstorage to create all localstorage objects needed in storage
    Added an initialisation feature to set all the localstorage objects needed. This creates all the objects that will need to be edited later.

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -5,5 +5,25 @@ import { Injectable } from '@angular/core';
 })
 export class StorageService {
 
-  constructor() { }
+  coords = undefined;
+  recipes = undefined;
+  favourites = undefined;
+
+  constructor(
+  ) {
+    if (!localStorage.getItem('recipes')) {
+      this.initialiseStorage();
+    }
+  }
+
+  initialiseStorage() {
+    localStorage.setItem('coords', this.coords);
+    localStorage.setItem('recipes', this.recipes);
+    localStorage.setItem('favourites', this.favourites);
+  }
+
+
+
+
+
 }
