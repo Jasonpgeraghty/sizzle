19d28d4f285d93d2de9eaabc34017f0732c9a245
Owner <Owner@Owners-MacBook-Pro.local>
2019-04-10 11:22:31 +0100

    added country and ingredients components to routes
    Added country and ingredients components to routes to allow the router to navigate to these components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { CategoryComponent } from './category/category.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { RecipeComponent } from './recipe/recipe.component';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { SearchComponent } from './search/search.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
+import { CountryComponent } from './country/country.component';
 
 const routes: Routes = [
   {path: '', component: CategoriesComponent},
   {path: 'category/:name', component: CategoryComponent},
   {path: 'recipe/:name', component: RecipeComponent},
-  {path: 'favourites', component: RecipeComponent},//to finish
-  {path: 'search', component: RecipeComponent},//to finish
-  {path: 'ingredients', component: RecipeComponent},//to finish
-  {path: 'country', component: RecipeComponent},//to finish
+  {path: 'favourites', component: FavouritesComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'ingredients', component: IngredientsComponent},
+  {path: 'country', component: CountryComponent},
   {path: '**', component: CategoriesComponent}//to finish
 ];
 
