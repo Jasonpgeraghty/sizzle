335be93771eb92c91307e20224e6b008c59d56bc
Owner <Owner@owners-mbp.home>
2019-04-12 16:07:57 +0100

    add method to allow stringified recipes to be added to storage
    Added method to allow a stringified recipe to be added to be retrieved from the recipes page.

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -7,6 +7,7 @@ export class StorageService {
 
   coords = undefined;
   recipes = undefined;
+  recipe = undefined;
   favourites = undefined;
 
   constructor(
@@ -19,7 +20,17 @@ export class StorageService {
   initialiseStorage() {
     localStorage.setItem('coords', this.coords);
     localStorage.setItem('recipes', this.recipes);
+    localStorage.setItem('recipes', this.recipe);
     localStorage.setItem('favourites', this.favourites);
+  }
+
+  setRecipes(recipes) {
+    localStorage.setItem('recipes', JSON.stringify(recipes));
+  }
+
+  setRecipe(recipe) {
+    localStorage.setItem('recipe', JSON.stringify(recipe));
+    return true;
   }
 
 
