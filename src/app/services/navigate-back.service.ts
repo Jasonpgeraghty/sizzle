00a0d91d5e177b3707efd0eb76cf37862bd37c11
Owner <Owner@owners-mbp.home>
2019-04-12 15:07:43 +0100

    modify service to allow user to navigate backwards
    Modify the service to allow the user to navigate back to their previous route. This will assist the user in navigating the website.

--- src/app/services/navigate-back.service.ts
+++ src/app/services/navigate-back.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavigateBackService {
 
-  constructor() { }
+  private lastRoute: string;
+  private currentRoute: string;
+
+  constructor(private router: Router) {
+    this.currentRoute = this.router.url;
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.lastRoute = this.currentRoute;
+        this.currentRoute = event.url;
+      }
+    });
+  }
+
+  getPreviousUrl() {
+    return this.lastRoute;
+  }
 }
