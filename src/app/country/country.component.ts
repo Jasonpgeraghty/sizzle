8a6d4f2be0e034f972590c90da196499a86fdce6
Owner <Owner@owners-mbp.home>
2019-04-13 16:21:54 +0100

    add ability to get countries from list of countries and added validation
    Added ability to get country from list and added validation to occur if the browser cannot get
    the users location.

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,23 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { LocationService } from '../services/location.service';
+import { CountriesService } from '../services/countries.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
-  styleUrls: ['./country.component.sass']
+  styleUrls: ['./country.component.scss']
 })
 export class CountryComponent implements OnInit {
 
   constructor(
-    private location: LocationService
+    private location: LocationService,
+    private countries: CountriesService,
+    private router: Router
   ) { }
 
-  ngOnInit() {
+  countryList: any;
+  selectedValue: string;
 
+  ngOnInit() {
+    this.getCountries();
+    this.selectedValue = '0';
+    console.log('Selected:' + this.selectedValue);
   }
 
   getLocation() {
-    this.location.getLocation();
+    if ('geolocation' in navigator) {
+      this.location.getLocation();
+    } else {
+      console.log('geolocation not active');
+    }
+  }
+
+  getCountries() {
+    this.countries
+    .getCountries()
+    .subscribe(
+      countryList => {
+        this.countryList = countryList;
+        console.log(this.countryList);
+      }
+    );
+  }
+
+  getRecipeForCountry() {
+    if (this.selectedValue !== '0') {
+      this.router.navigate([`category/${this.selectedValue.split(' ').join('-')}`]);
+    } else {
+      console.log("can't be initial value");
+    }
   }
 
 }
