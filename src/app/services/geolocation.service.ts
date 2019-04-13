d28f7aa31447d5729778ac69003c3d1ce2a33e8f
Owner <Owner@owners-mbp.home>
2019-04-13 19:07:30 +0100

    create a method to allow user to send coords to geolocation API to retrieve country code
    Creates a method that allows the user to send their coordinates to retrieve their country code, this will
    then we compared to a local JSON file to get the countries full name and search for recipes for it.

--- src/app/services/geolocation.service.ts
+++ src/app/services/geolocation.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeolocationService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  API_KEY = 'dJAlGh6EyrzkUo0Fch4VENGfP2yZO01H';
+
+  getLocation(long: string, lat: string) {
+    return this.http.get(`http://www.mapquestapi.com/geocoding/v1/reverse?key=${this.API_KEY}&location=${lat},${long}`);
+  }
 }
