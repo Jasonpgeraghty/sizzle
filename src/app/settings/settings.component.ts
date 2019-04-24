2eba0dee6e407c6b3132e2f4e125e8064371d753
Owner <Owner@owners-mbp.home>
2019-04-24 16:24:48 +0100

    add ability to toggle between langauges
    Added the ability for the user to toggle what language they would like to view the content in. They can view english or french.

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private translate: TranslateService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onChange(value) {
+    this.translate.use(value);
   }
 
 }
