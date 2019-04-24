75a1e52e6d4f430efecbdc443ff5e128f303ce49
Owner <Owner@owners-mbp.home>
2019-04-24 18:24:34 +0100

    filter information from URL that will no be able to load
    Additional symbols were present in the names of the recipes that would have caused issues
    with the browser.

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -33,7 +33,6 @@ export class CategoryComponent implements OnInit {
       this.spinner.hide();
      }
 
-     console.log('1.' + this.recipes);
      this.activeRoute.paramMap.subscribe(params => {
 
       const recipe = params.get('name');
@@ -43,14 +42,11 @@ export class CategoryComponent implements OnInit {
       const storedRecipeName = storedRecipe.q;
 
       if (storedRecipeName !== recipe) {
-        console.log('http');
-        console.log('2.' + this.recipes);
         this.recipeService.searchRecipes(recipe)
         .subscribe(
           (recipes: any) => {
             if (recipes !== undefined) {
               this.recipes = recipes.hits;
-              console.log(this.recipes);
               this.storage.setRecipes(recipes);
               setTimeout(() => {
                 this.spinner.hide();
@@ -58,9 +54,7 @@ export class CategoryComponent implements OnInit {
             }
         });
       } else {
-        console.log('storage');
         this.recipes = storedRecipe.hits;
-        console.log('2.' + this.recipes);
         setTimeout(() => {
           this.spinner.hide();
         }, 1500);
@@ -69,10 +63,12 @@ export class CategoryComponent implements OnInit {
   }
 
   goToRecipe(recipe) {
-    console.log(recipe);
     if (this.storage.setRecipe(recipe)) {
       this.router.navigate([`recipe/${recipe.label
                             .split(' ').join('-')
+                            .split('/').join('-')
+                            .split('(').join('-')
+                            .split(')').join('-')
                             .split('/').join('-')}`]);
     }
   }
