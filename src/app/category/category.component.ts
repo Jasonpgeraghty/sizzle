0acaec19d81751bae40a425d63c2fdb2faad456d
Owner <Owner@owners-mbp.home>
2019-04-24 14:04:09 +0100

    add ngx-spinner to appear when content is loading
    Added ngx-spinner that will appear when content loads and add ability to save data in localstorage.

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RecipesService } from '../services/recipes.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { StorageService } from '../services/storage.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-category',
@@ -17,29 +18,54 @@ export class CategoryComponent implements OnInit {
     private recipeService: RecipesService,
     private activeRoute: ActivatedRoute,
     private router: Router,
-    private storage: StorageService
+    private storage: StorageService,
+    private spinner: NgxSpinnerService
   ) {
-
-    this.recipes = null;
-
-    this.activeRoute.paramMap.subscribe(params => {
-
-    const recipe = params.get('name');
-
-    this.recipeService.searchRecipes(recipe)
-    .subscribe(
-      (recipes: any) => {
-        if (recipes !== undefined) {
-          console.log(recipes);
-          this.recipes = recipes.hits;
-        }
-    });
-    });
   }
 
 
   ngOnInit() {
+     this.recipes = null;
 
+     if (this.recipes === null) {
+         this.spinner.show();
+     } else {
+      this.spinner.hide();
+     }
+
+     console.log('1.' + this.recipes);
+     this.activeRoute.paramMap.subscribe(params => {
+
+      const recipe = params.get('name');
+
+      const storedRecipe = this.storage.getRecipes();
+
+      const storedRecipeName = storedRecipe.q;
+
+      if (storedRecipeName !== recipe) {
+        console.log('http');
+        console.log('2.' + this.recipes);
+        this.recipeService.searchRecipes(recipe)
+        .subscribe(
+          (recipes: any) => {
+            if (recipes !== undefined) {
+              this.recipes = recipes.hits;
+              console.log(this.recipes);
+              this.storage.setRecipes(recipes);
+              setTimeout(() => {
+                this.spinner.hide();
+              }, 1500);
+            }
+        });
+      } else {
+        console.log('storage');
+        this.recipes = storedRecipe.hits;
+        console.log('2.' + this.recipes);
+        setTimeout(() => {
+          this.spinner.hide();
+        }, 1500);
+      }
+    });
   }
 
   goToRecipe(recipe) {
